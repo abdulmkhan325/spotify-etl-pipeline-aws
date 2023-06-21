6bb6f649827a0672404567896c84fb3bfb36fdb9
abdulmkhan325 <abdulmkhan325@gmail.com>
2023-06-21 18:00:21 +1000

    Initial commit

---
+++ spotify.py
@@ -0,0 +1,8 @@
+# Using Python lib spotipy 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+user_spotify_credentials = SpotifyClientCredentials(client_id='84fd22c3658a41e4b72a03597f13b388',client_secret='1b9f47969dec4627a0a1f08ebb6eced4')
+sp = spotipy.Spotify(client_credentials_manager=user_spotify_credentials)
+
+print(sp.user(user='22knxy7shmeqilqxlktxwwldq'))
