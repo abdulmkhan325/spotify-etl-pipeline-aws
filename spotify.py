02e0907d51dcf61015e1a19e1fa8e41feabd9235
abdulmkhan325 <abdulmkhan325@gmail.com>
2023-06-21 20:02:05 +1000

    Just created a new branch and merged it

--- spotify.py
+++ spotify.py
@@ -2,9 +2,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-user_spotify_credentials = SpotifyClientCredentials(client_id='84fd22c3658a41e4b72a03597f13b388',client_secret='1b9f47969dec4627a0a1f08ebb6eced4')
+user_spotify_credentials = SpotifyClientCredentials(client_id='ABC',client_secret='XYZ')
 sp = spotipy.Spotify(client_credentials_manager=user_spotify_credentials)
 
-print(sp.user(user='22knxy7shmeqilqxlktxwwldq'))
+print(sp)
 
 # End
