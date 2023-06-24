0daedab159f028c1b6792e132f57a17deabcf8b5
abdulmkhan325 <abdulmkhan325@gmail.com>
2023-06-24 17:57:30 +1000

    updated spotify uri

--- spotify.py
+++ spotify.py
@@ -1,10 +1,14 @@
-# Using Python lib spotipy 
+# Using Python lib spotipy
+import Credentials as cr
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-user_spotify_credentials = SpotifyClientCredentials(client_id='ABC',client_secret='XYZ')
+user_spotify_credentials = SpotifyClientCredentials(client_id=cr.my_client_id,client_secret=cr.my_client_secret)
+
 sp = spotipy.Spotify(client_credentials_manager=user_spotify_credentials)
 
-print(sp)
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF"
+playlist_uri = playlist_link.split('/')[4]
 
-# End of changes
+
+print(sp.playlist_tracks(playlist_id=playlist_uri))
