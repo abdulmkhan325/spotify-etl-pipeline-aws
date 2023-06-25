feea241f88e6edeef4c9298a58ac38c2419780d3
abdulmkhan325 <abdulmkhan325@gmail.com>
2023-06-25 21:00:09 +1000

    Used pandas dataframe to convert album artists and songs

--- spotify.py
+++ spotify.py
@@ -2,6 +2,7 @@
 import spotipy
 import Credentials as cr 
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
 
 # Using credentials to access Spotify Web API  
 user_spotify_credentials = SpotifyClientCredentials(client_id=cr.my_client_id,client_secret=cr.my_client_secret)
@@ -42,4 +43,41 @@ for row in data['items']:
     for key, value in row.items():
         if(key == 'track'):
             for artist in value['artists']: 
-                print(artist['name'])
+                artist_id = artist['id']
+                artist_name = artist['name']
+                artist_url = artist['href']
+                artist_dict = {'artist_id': artist_id , 'artist_name': artist_name, 'artist_url': artist_url}  
+                artist_list.append(artist_dict)
+
+# songs list will be used to store dictionaries 
+# representing list of songs
+songs_list = [] 
+for row in data['items']:
+    song_id = row['track']['id']
+    song_name = row['track']['name']
+    song_duration = row['track']['duration_ms']
+    song_url = row['track']['external_urls']['spotify']
+    song_popularity = row['track']['popularity']
+    song_added = row['added_at']
+    album_id = row['track']['album']['id']
+    artist_id = row['track']['album']['artists'][0]['id']
+    songs_dict = {'song_id': song_id , 'song_name': song_name, 'song_duration': song_duration,
+                  'song_url': song_url , 'song_popularity': song_popularity, 'song_added': song_added,
+                  'album_id': album_id , 'artist_id': artist_id}  
+    songs_list.append(songs_dict)
+
+
+album_df = pd.DataFrame.from_dict(album_list)
+album_df = album_df.drop_duplicates(subset=['album_id']) 
+album_df['album_release_date'] = pd.to_datetime(album_df['album_release_date'])
+
+
+artist_df = pd.DataFrame.from_dict(artist_list)
+artist_df = artist_df.drop_duplicates(subset=['artist_id'])  
+
+
+songs_df = pd.DataFrame.from_dict(songs_list)
+songs_df = songs_df.drop_duplicates(subset=['song_id']) 
+songs_df['song_added'] = pd.to_datetime(songs_df['song_added'])
+
+print(songs_df.info())
